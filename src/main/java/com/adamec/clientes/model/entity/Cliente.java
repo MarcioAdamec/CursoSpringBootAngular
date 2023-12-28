62ce94ac86d1142f4747eca3e93365ec6b0074ae
marcio.adamec <marcio.adamec@gmail.com>
2023-12-28 09:06:03 -0300

    0.6

--- src/main/java/com/adamec/clientes/model/entity/Cliente.java
+++ src/main/java/com/adamec/clientes/model/entity/Cliente.java
@@ -1,23 +1,47 @@
 package com.adamec.clientes.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
 @Data //Cria Geters e Seters e Construtores - Lombok
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+
     @Column(nullable = false, length = 150)
+    @NotEmpty(message = "{campo.nome.obrigatorio}")
+    @NotNull
     private String nome;
+
     @Column(nullable = false, length = 11)
+    @NotNull
+    //@CPF(message = "{campo.cpf.invalido}")
     private String cpf;
-    @Column
+
+    @Column(name = "data_do_cadastro", updatable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCadastro;
 
+    @PrePersist
+    public void prePersist(){
+        setDataCadastro(LocalDate.now());
+    }
 
 }
